2f18322bd3f00658d443a1c81ebf67c5a260592d
liminat <180078053+liminat@users.noreply.github.com>
2022-09-21 13:22:23 +0000

    last

--- machine_translation/nmt/bleu.py
+++ machine_translation/nmt/bleu.py
@@ -167,4 +167,50 @@ def compute_bleu(reference_corpus_list, translation_corpus, tokenized=True,
         list of list(str): plain text
         List of references for each translation.
     translation_corpus: list(list(str)) or list(str)
-        list(list(str)
+        list(list(str)): tokenized translation
+        list(str): plain text
+        Translations to score.
+    tokenized: bool, default True
+        Whether the inputs has been tokenized.
+    tokenizer: str or None, default '13a'
+        '13a': follow the tokenizer in mteval-v13a.pl
+        'intl': follow the international tokenizer in mteval-v14.pl
+        None: identity mapping on the string.
+        This option is ignored if tokenized is True
+    max_n: int, default 4
+        Maximum n-gram order to use when computing BLEU score.
+    smooth: bool, default False
+        Whether or not to compute smoothed bleu score.
+    lower_case: bool, default False
+        Whether or not to use lower case of tokens
+    split_compound_word: bool, default False
+        Whether or not to split compound words
+        "rich-text format" --> rich ##AT##-##AT## text format.
+    bpe: bool, default False
+        Whether or not the inputs are in BPE format
+
+    Returns
+    -------
+    5-Tuple with the BLEU score, n-gram precisions, brevity penalty,
+        reference length, and translation length
+    """
+    precision_numerators = [0 for _ in range(max_n)]
+    precision_denominators = [0 for _ in range(max_n)]
+    ref_length, trans_length = 0, 0
+    for references in reference_corpus_list:
+        assert len(references) == len(translation_corpus), \
+            'The number of translations and their references do not match'
+    if tokenized:
+        assert isinstance(reference_corpus_list[0][0], LIST_TYPES) and \
+               isinstance(translation_corpus[0], LIST_TYPES), \
+            'references and translation should have format of list of list(list(str)) ' \
+            'and list(list(str)), respectively, when tokenized is True.'
+    else:
+        assert isinstance(reference_corpus_list[0][0], six.string_types) and \
+               isinstance(translation_corpus[0], six.string_types), \
+            'references and translation should have format of list(list(str)) ' \
+            'and list(str), respectively, when tokenized is False.'
+    for references, translation in zip(zip(*reference_corpus_list), translation_corpus):
+        if not tokenized:
+            references = [TOKENIZERS[tokenizer](reference).split() for reference in references]
+            translation = TOK
