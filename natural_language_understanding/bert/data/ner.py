aed37e97e1877ef7ced3c1cb8947c589eece7132
liminat <180078053+liminat@users.noreply.github.com>
2022-11-30 14:49:38 +0000

    cmake: sync submodule

--- natural_language_understanding/bert/data/ner.py
+++ natural_language_understanding/bert/data/ner.py
@@ -48,4 +48,65 @@ def bio_bioes(tokens):
         if token.tag == 'O':
             ret.append(token)
         elif token.tag.startswith('B'):
-      
+            # if a B-tag is continued by other tokens with the same entity,
+            # then it is still a B-tag
+            if index + 1 < len(tokens) and tokens[index + 1].tag.startswith('I'):
+                ret.append(token)
+            else:
+                ret.append(TaggedToken(text=token.text, tag='S' + token.tag[1:]))
+        elif token.tag.startswith('I'):
+            # if an I-tag is continued by other tokens with the same entity,
+            # then it is still an I-tag
+            if index + 1 < len(tokens) and tokens[index + 1].tag.startswith('I'):
+                ret.append(token)
+            else:
+                ret.append(TaggedToken(text=token.text, tag='E' + token.tag[1:]))
+    return ret
+
+
+def read_bio_as_bio2(data_path):
+    """Read CoNLL-formatted text file in BIO scheme in given path as sentences in BIO2 scheme.
+
+    Parameters
+    ----------
+    data_path: str
+        Path of the data file to read
+
+    Returns
+    -------
+    List[List[TaggedToken]]:
+        List of sentences, each of which is a List of TaggedTokens
+    """
+
+    with open(data_path, 'r') as ifp:
+        sentence_list = []
+        current_sentence = []
+        prev_tag = 'O'
+
+        for line in ifp:
+            if len(line.strip()) > 0:
+                word, _, _, tag = line.rstrip().split(' ')
+                # convert BIO tag to BIO2 tag
+                if tag == 'O':
+                    bio2_tag = 'O'
+                else:
+                    if prev_tag == 'O' or tag[2:] != prev_tag[2:]:
+                        bio2_tag = 'B' + tag[1:]
+                    else:
+                        bio2_tag = tag
+                current_sentence.append(TaggedToken(text=word, tag=bio2_tag))
+                prev_tag = tag
+            else:
+                # the sentence was completed if an empty line occurred; flush the current sentence.
+                sentence_list.append(current_sentence)
+                current_sentence = []
+                prev_tag = 'O'
+
+        # check if there is a remaining token. in most CoNLL data files, this does not happen.
+        if len(current_sentence) > 0:
+            sentence_list.append(current_sentence)
+        return sentence_list
+
+
+def remove_docstart_sentence(sentences):
+    """Remove -DOCSTART- sen
