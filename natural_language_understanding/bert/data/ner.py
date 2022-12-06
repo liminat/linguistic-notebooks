bbd91c4db94104bf5821a6f45c745a2e6362bb70
liminat <180078053+liminat@users.noreply.github.com>
2022-12-06 13:01:20 +0000

    No comments

--- natural_language_understanding/bert/data/ner.py
+++ natural_language_understanding/bert/data/ner.py
@@ -236,4 +236,53 @@ class BERTTaggingDataset(object):
                 logging.info(str(test_sentences[i]))
 
         self.train_inputs = [self._encode_as_input(sentence) for sentence in train_sentences]
-        sel
+        self.dev_inputs = [self._encode_as_input(sentence) for sentence in dev_sentences]
+        self.test_inputs = [self._encode_as_input(sentence) for sentence in test_sentences]
+
+        logging.info('tag_vocab: %s', self.tag_vocab)
+
+    def _encode_as_input(self, sentence):
+        """Enocde a single sentence into numpy arrays as input to the BERTTagger model.
+
+        Parameters
+        ----------
+        sentence: List[TaggedToken]
+            A sentence as a list of tagged tokens.
+
+        Returns
+        -------
+        np.array: token text ids (batch_size, seq_len)
+        np.array: token types (batch_size, seq_len),
+                which is all zero because we have only one sentence for tagging.
+        np.array: valid_length (batch_size,) the number of tokens until [SEP] token
+        np.array: tag_ids (batch_size, seq_len)
+        np.array: flag_nonnull_tag (batch_size, seq_len),
+                which is simply tag_ids != self.null_tag_index
+
+        """
+        # check whether the given sequence can be fit into `seq_len`.
+        assert len(sentence) <= self.seq_len - 2, \
+            'the number of tokens {} should not be larger than {} - 2. offending sentence: {}' \
+            .format(len(sentence), self.seq_len, sentence)
+
+        text_tokens = ([self.text_vocab.cls_token] + [token.text for token in sentence] +
+                       [self.text_vocab.sep_token])
+        padded_text_ids = (self.text_vocab.to_indices(text_tokens)
+                           + ([self.text_vocab[self.text_vocab.padding_token]]
+                              * (self.seq_len - len(text_tokens))))
+
+        tags = [NULL_TAG] + [token.tag for token in sentence] + [NULL_TAG]
+        padded_tag_ids = (self.tag_vocab.to_indices(tags)
+                          + [self.tag_vocab[NULL_TAG]] * (self.seq_len - len(tags)))
+
+        assert len(text_tokens) == len(tags)
+        assert len(padded_text_ids) == len(padded_tag_ids)
+        assert len(padded_text_ids) == self.seq_len
+
+        valid_length = len(text_tokens)
+
+        # in sequence tagging problems, only one sentence is given
+        token_types = [0] * self.seq_len
+
+        np_tag_ids = np.array(padded_tag_ids, dtype='int32')
+        # gluon batchify cannot batchify numpy.bool
