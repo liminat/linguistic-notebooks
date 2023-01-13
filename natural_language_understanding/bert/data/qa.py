d49d57fe0f550acd99d8c466d4bda6fd79ea618c
liminat <180078053+liminat@users.noreply.github.com>
2023-01-13 10:12:06 +0000

    net: wireless: bcmdhd: Add support for hidden ssid PNO scan

--- natural_language_understanding/bert/data/qa.py
+++ natural_language_understanding/bert/data/qa.py
@@ -508,4 +508,14 @@ def _check_is_max_context(doc_spans, cur_span_index, position):
         end = doc_span.start + doc_span.length - 1
         if position < doc_span.start:
             continue
-        if position > 
+        if position > end:
+            continue
+        num_left_context = position - doc_span.start
+        num_right_context = end - position
+        score = min(num_left_context, num_right_context) + \
+            0.01 * doc_span.length
+        if best_score is None or score > best_score:
+            best_score = score
+            best_span_index = span_index
+
+    return cur_span_index == best_span_index
---
+++ natural_language_understanding/bert/export/export.py
@@ -0,0 +1,21 @@
+"""
+Export the BERT Model for Deployment
+
+====================================
+
+This script exports the BERT model to a hybrid model serialized as a symbol.json file,
+which is suitable for deployment, or use with MXNet Module API.
+
+@article{devlin2018bert,
+  title={BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding},
+  author={Devlin, Jacob and Chang, Ming- \
+      Wei and Lee, Kenton and Toutanova, Kristina},
+  journal={arXiv preprint arXiv:1810.04805},
+  year={2018}
+}
+"""
+
+# coding=utf-8
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license 
