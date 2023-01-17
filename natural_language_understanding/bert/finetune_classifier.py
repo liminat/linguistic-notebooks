5d099e830d3832b8ec3df8bb499f25995f04e240
liminat <180078053+liminat@users.noreply.github.com>
2023-01-17 17:04:06 +0000

    fix doc.

--- natural_language_understanding/bert/finetune_classifier.py
+++ natural_language_understanding/bert/finetune_classifier.py
@@ -222,3 +222,322 @@ bert, vocabulary = get_model(
     use_classifier=False)
 
 if not task.class_labels:
+    # STS-B is a regression task.
+    # STSBTask().class_labels returns None
+    model = BERTRegression(bert, dropout=0.1)
+    if not model_parameters:
+        model.regression.initialize(init=mx.init.Normal(0.02), ctx=ctx)
+    loss_function = gluon.loss.L2Loss()
+else:
+    model = BERTClassifier(
+        bert, dropout=0.1, num_classes=len(task.class_labels))
+    if not model_parameters:
+        model.classifier.initialize(init=mx.init.Normal(0.02), ctx=ctx)
+    loss_function = gluon.loss.SoftmaxCELoss()
+
+# load checkpointing
+output_dir = args.output_dir
+if pretrained_bert_parameters:
+    logging.info('loading bert params from %s', pretrained_bert_parameters)
+    model.bert.load_parameters(pretrained_bert_parameters, ctx=ctx,
+                               ignore_extra=True)
+if model_parameters:
+    logging.info('loading model params from %s', model_parameters)
+    model.load_parameters(model_parameters, ctx=ctx)
+nlp.utils.mkdir(output_dir)
+
+logging.debug(model)
+model.hybridize(static_alloc=True)
+loss_function.hybridize(static_alloc=True)
+
+# data processing
+do_lower_case = 'uncased' in dataset
+bert_tokenizer = BERTTokenizer(vocabulary, lower=do_lower_case)
+
+def preprocess_data(tokenizer, task, batch_size, dev_batch_size, max_len, pad=False):
+    """Train/eval Data preparation function."""
+    pool = multiprocessing.Pool()
+
+    # transformation for data train and dev
+    label_dtype = 'float32' if not task.class_labels else 'int32'
+    trans = BERTDatasetTransform(tokenizer, max_len,
+                                 class_labels=task.class_labels,
+                                 label_alias=task.label_alias,
+                                 pad=pad, pair=task.is_pair,
+                                 has_label=True)
+
+    # data train
+    # task.dataset_train returns (segment_name, dataset)
+    train_tsv = task.dataset_train()[1]
+    data_train = mx.gluon.data.SimpleDataset(pool.map(trans, train_tsv))
+    data_train_len = data_train.transform(
+        lambda input_id, length, segment_id, label_id: length, lazy=False)
+    # bucket sampler for training
+    batchify_fn = nlp.data.batchify.Tuple(
+        nlp.data.batchify.Pad(axis=0), nlp.data.batchify.Stack(),
+        nlp.data.batchify.Pad(axis=0), nlp.data.batchify.Stack(label_dtype))
+    batch_sampler = nlp.data.sampler.FixedBucketSampler(
+        data_train_len,
+        batch_size=batch_size,
+        num_buckets=10,
+        ratio=0,
+        shuffle=True)
+    # data loader for training
+    loader_train = gluon.data.DataLoader(
+        dataset=data_train,
+        num_workers=1,
+        batch_sampler=batch_sampler,
+        batchify_fn=batchify_fn)
+
+    # data dev. For MNLI, more than one dev set is available
+    dev_tsv = task.dataset_dev()
+    dev_tsv_list = dev_tsv if isinstance(dev_tsv, list) else [dev_tsv]
+    loader_dev_list = []
+    for segment, data in dev_tsv_list:
+        data_dev = mx.gluon.data.SimpleDataset(pool.map(trans, data))
+        loader_dev = mx.gluon.data.DataLoader(
+            data_dev,
+            batch_size=dev_batch_size,
+            num_workers=1,
+            shuffle=False,
+            batchify_fn=batchify_fn)
+        loader_dev_list.append((segment, loader_dev))
+
+    # batchify for data test
+    test_batchify_fn = nlp.data.batchify.Tuple(
+        nlp.data.batchify.Pad(axis=0), nlp.data.batchify.Stack(),
+        nlp.data.batchify.Pad(axis=0))
+    # transform for data test
+    test_trans = BERTDatasetTransform(tokenizer, max_len,
+                                      class_labels=None,
+                                      pad=pad, pair=task.is_pair,
+                                      has_label=False)
+
+    # data test. For MNLI, more than one test set is available
+    test_tsv = task.dataset_test()
+    test_tsv_list = test_tsv if isinstance(test_tsv, list) else [test_tsv]
+    loader_test_list = []
+    for segment, data in test_tsv_list:
+        data_test = mx.gluon.data.SimpleDataset(pool.map(test_trans, data))
+        loader_test = mx.gluon.data.DataLoader(
+            data_test,
+            batch_size=dev_batch_size,
+            num_workers=1,
+            shuffle=False,
+            batchify_fn=test_batchify_fn)
+        loader_test_list.append((segment, loader_test))
+    return loader_train, loader_dev_list, loader_test_list, len(data_train)
+
+
+# Get the loader.
+logging.info('processing dataset...')
+train_data, dev_data_list, test_data_list, num_train_examples = preprocess_data(
+    bert_tokenizer, task, batch_size, dev_batch_size, args.max_len, args.pad)
+
+
+def test(loader_test, segment):
+    """Inference function on the test dataset."""
+    logging.info('Now we are doing testing on %s with %s.', segment, ctx)
+
+    tic = time.time()
+    results = []
+    for _, seqs in enumerate(loader_test):
+        input_ids, valid_length, type_ids = seqs
+        out = model(input_ids.as_in_context(ctx),
+                    type_ids.as_in_context(ctx),
+                    valid_length.astype('float32').as_in_context(ctx))
+        if not task.class_labels:
+            # regression task
+            for result in out.asnumpy().reshape(-1).tolist():
+                results.append('{:.3f}'.format(result))
+        else:
+            # classification task
+            indices = mx.nd.topk(out, k=1, ret_typ='indices', dtype='int32').asnumpy()
+            for index in indices:
+                results.append(task.class_labels[int(index)])
+
+    mx.nd.waitall()
+    toc = time.time()
+    logging.info('Time cost=%.2fs, throughput=%.2f samples/s', toc - tic,
+                 dev_batch_size * len(loader_test) / (toc - tic))
+    # write result to a file.
+    filename = args.task_name + segment.replace('test', '') + '.csv'
+    test_path = os.path.join(args.output_dir, filename)
+    with io.open(test_path, 'w', encoding='utf-8') as f:
+        f.write(u'index\tprediction\n')
+        for i, pred in enumerate(results):
+            f.write(u'%d\t%s\n'%(i, str(pred)))
+
+
+def log_train(batch_id, batch_num, metric, step_loss, log_interval, epoch_id, learning_rate):
+    """Generate and print out the log message for training. """
+    metric_nm, metric_val = metric.get()
+    if not isinstance(metric_nm, list):
+        metric_nm, metric_val = [metric_nm], [metric_val]
+
+    train_str = '[Epoch %d Batch %d/%d] loss=%.4f, lr=%.7f, metrics:' + \
+                ','.join([i + ':%.4f' for i in metric_nm])
+    logging.info(train_str, epoch_id + 1, batch_id + 1, batch_num,
+                 step_loss / log_interval, learning_rate, *metric_val)
+
+
+def log_eval(batch_id, batch_num, metric, step_loss, log_interval):
+    """Generate and print out the log message for inference. """
+    metric_nm, metric_val = metric.get()
+    if not isinstance(metric_nm, list):
+        metric_nm, metric_val = [metric_nm], [metric_val]
+
+    eval_str = '[Batch %d/%d] loss=%.4f, metrics:' + \
+               ','.join([i + ':%.4f' for i in metric_nm])
+    logging.info(eval_str, batch_id + 1, batch_num,
+                 step_loss / log_interval, *metric_val)
+
+
+def train(metric):
+    """Training function."""
+    if not only_inference:
+        logging.info('Now we are doing BERT classification training on %s!', ctx)
+
+    all_model_params = model.collect_params()
+    optimizer_params = {'learning_rate': lr, 'epsilon': epsilon, 'wd': 0.01}
+    try:
+        trainer = gluon.Trainer(all_model_params, args.optimizer,
+                                optimizer_params, update_on_kvstore=False)
+    except ValueError as e:
+        print(e)
+        warnings.warn(
+            'AdamW optimizer is not found. Please consider upgrading to '
+            'mxnet>=1.5.0. Now the original Adam optimizer is used instead.')
+        trainer = gluon.Trainer(all_model_params, 'adam',
+                                optimizer_params, update_on_kvstore=False)
+
+    step_size = batch_size * accumulate if accumulate else batch_size
+    num_train_steps = int(num_train_examples / step_size * args.epochs)
+    warmup_ratio = args.warmup_ratio
+    num_warmup_steps = int(num_train_steps * warmup_ratio)
+    step_num = 0
+
+    # Do not apply weight decay on LayerNorm and bias terms
+    for _, v in model.collect_params('.*beta|.*gamma|.*bias').items():
+        v.wd_mult = 0.0
+    # Collect differentiable parameters
+    params = [p for p in all_model_params.values() if p.grad_req != 'null']
+
+    # Set grad_req if gradient accumulation is required
+    if accumulate:
+        for p in params:
+            p.grad_req = 'add'
+    # track best eval score
+    metric_history = []
+
+    tic = time.time()
+    for epoch_id in range(args.epochs):
+        if not only_inference:
+            metric.reset()
+            step_loss = 0
+            tic = time.time()
+            all_model_params.zero_grad()
+
+            for batch_id, seqs in enumerate(train_data):
+                # learning rate schedule
+                if step_num < num_warmup_steps:
+                    new_lr = lr * step_num / num_warmup_steps
+                else:
+                    non_warmup_steps = step_num - num_warmup_steps
+                    offset = non_warmup_steps / (num_train_steps - num_warmup_steps)
+                    new_lr = lr - offset * lr
+                trainer.set_learning_rate(new_lr)
+
+                # forward and backward
+                with mx.autograd.record():
+                    input_ids, valid_length, type_ids, label = seqs
+                    out = model(
+                        input_ids.as_in_context(ctx), type_ids.as_in_context(ctx),
+                        valid_length.astype('float32').as_in_context(ctx))
+                    ls = loss_function(out, label.as_in_context(ctx)).mean()
+                ls.backward()
+
+                # update
+                if not accumulate or (batch_id + 1) % accumulate == 0:
+                    trainer.allreduce_grads()
+                    nlp.utils.clip_grad_global_norm(params, 1)
+                    trainer.update(accumulate if accumulate else 1)
+                    # set grad to zero for gradient accumulation
+                    all_model_params.zero_grad()
+                    step_num += 1
+
+                step_loss += ls.asscalar()
+                metric.update([label], [out])
+                if (batch_id + 1) % (args.log_interval) == 0:
+                    log_train(batch_id, len(train_data), metric, step_loss, args.log_interval,
+                              epoch_id, trainer.learning_rate)
+                    step_loss = 0
+            mx.nd.waitall()
+
+        # inference on dev data
+        for segment, dev_data in dev_data_list:
+            metric_nm, metric_val = evaluate(dev_data, metric, segment)
+            metric_history.append((epoch_id, metric_nm, metric_val))
+
+        if not only_inference:
+            # save params
+            ckpt_name = 'model_bert_{0}_{1}.params'.format(task_name, epoch_id)
+            params_saved = os.path.join(output_dir, ckpt_name)
+
+            nlp.utils.save_parameters(model, params_saved)
+            logging.info('params saved in: %s', params_saved)
+            toc = time.time()
+            logging.info('Time cost=%.2fs', toc - tic)
+            tic = toc
+
+    if not only_inference:
+        # we choose the best model based on metric[0],
+        # assuming higher score stands for better model quality
+        metric_history.sort(key=lambda x: x[2][0], reverse=True)
+        epoch_id, metric_nm, metric_val = metric_history[0]
+        ckpt_name = 'model_bert_{0}_{1}.params'.format(task_name, epoch_id)
+        params_saved = os.path.join(output_dir, ckpt_name)
+        nlp.utils.load_parameters(model, params_saved)
+        metric_str = 'Best model at epoch {}. Validation metrics:'.format(epoch_id)
+        metric_str += ','.join([i + ':%.4f' for i in metric_nm])
+        logging.info(metric_str, *metric_val)
+
+    # inference on test data
+    for segment, test_data in test_data_list:
+        test(test_data, segment)
+
+def evaluate(loader_dev, metric, segment):
+    """Evaluate the model on validation dataset."""
+    logging.info('Now we are doing evaluation on %s with %s.', segment, ctx)
+    metric.reset()
+    step_loss = 0
+    tic = time.time()
+    for batch_id, seqs in enumerate(loader_dev):
+        input_ids, valid_len, type_ids, label = seqs
+        out = model(
+            input_ids.as_in_context(ctx), type_ids.as_in_context(ctx),
+            valid_len.astype('float32').as_in_context(ctx))
+        ls = loss_function(out, label.as_in_context(ctx)).mean()
+
+        step_loss += ls.asscalar()
+        metric.update([label], [out])
+
+        if (batch_id + 1) % (args.log_interval) == 0:
+            log_eval(batch_id, len(loader_dev), metric, step_loss, args.log_interval)
+            step_loss = 0
+
+    metric_nm, metric_val = metric.get()
+    if not isinstance(metric_nm, list):
+        metric_nm, metric_val = [metric_nm], [metric_val]
+    metric_str = 'validation metrics:' + ','.join([i + ':%.4f' for i in metric_nm])
+    logging.info(metric_str, *metric_val)
+
+    mx.nd.waitall()
+    toc = time.time()
+    logging.info('Time cost=%.2fs, throughput=%.2f samples/s', toc - tic,
+                 dev_batch_size * len(loader_dev) / (toc - tic))
+    return metric_nm, metric_val
+
+
+if __name__ == '__main__':
+    train(task.metrics)
---
+++ natural_language_understanding/bert/index.rst
@@ -0,0 +1,16 @@
+Bidirectional Encoder Representations from Transformers
+-------------------------------------------------------
+
+:download:`Download scripts </model_zoo/bert.zip>`
+
+Reference: Devlin, Jacob, et al. "`Bert: Pre-training of deep bidirectional transformers for language understanding. <https://arxiv.org/abs/1810.04805>`_" arXiv preprint arXiv:1810.04805 (2018).
+
+Note: BERT model requires `nightly version of MXNet <https://mxnet.incubator.apache.org/versions/master/install/index.html?version=master&platform=Linux&language=Python&processor=CPU>`__. 
+
+The following pre-trained BERT models are available from the **gluonnlp.model.get_model** API:
+
++-------------------------------+----------------+-----------------+
+|                               | bert_12_768_12 | bert_24_1024_16 |
++===============================+================+=================+
+| book_corpus_wiki_en_uncased   | ✓              | ✓               |
++-------------------------------+----------------+------------
