d06728f799609698c648fdcab429a163bb8962a8
liminat <180078053+liminat@users.noreply.github.com>
2023-03-23 18:12:51 +0000

    Console commit

--- natural_language_understanding/bert/run_pretraining.py
+++ natural_language_understanding/bert/run_pretraining.py
@@ -14,4 +14,67 @@ This example shows how to pre-train a BERT model with Gluon NLP Toolkit.
 
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
-# distribute
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# pylint:disable=redefined-outer-name,logging-format-interpolation
+
+import os
+import logging
+import time
+
+import mxnet as mx
+import gluonnlp as nlp
+
+from utils import profile
+from fp16_utils import FP16Trainer
+from pretraining_utils import get_model_loss, get_pretrain_data_npz, get_dummy_dataloader
+from pretraining_utils import log, evaluate, forward, split_and_load, get_argparser
+from pretraining_utils import save_parameters, save_states
+
+# arg parser
+parser = get_argparser()
+parser.add_argument('--gpus', type=str, default='0', help='List of GPUs to use. e.g. 1,3')
+parser.add_argument('--kvstore', type=str, default='device', help='KVStore type')
+args = parser.parse_args()
+
+os.environ['MXNET_KVSTORE_USETREE'] = '1'
+os.environ['MXNET_GPU_MEM_POOL_TYPE'] = 'Round'
+
+# logging
+level = logging.DEBUG if args.verbose else logging.INFO
+logging.getLogger().setLevel(level)
+logging.info(args)
+
+class ParallelBERT(nlp.utils.Parallelizable):
+    """Data parallel BERT model.
+
+    Parameters
+    ----------
+    model : Block
+        The BERT model.
+    """
+    def __init__(self, model, mlm_loss, nsp_loss, vocab_size, rescale_factor, trainer=None):
+        self._model = model
+        self._mlm_loss = mlm_loss
+        self._nsp_loss = nsp_loss
+        self._vocab_size = vocab_size
+        self._rescale_factor = rescale_factor
+        self._trainer = trainer
+
+    def forward_backward(self, x):
+        """forward backward implementation"""
+        with mx.autograd.record():
+            (ls, next_sentence_label, classified, masked_id, decoded, \
+             masked_weight, ls1, ls2, valid_length) = fo
