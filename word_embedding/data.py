4c3e87453bacef79d6ad069bfc422685f98c3eea
liminat <180078053+liminat@users.noreply.github.com>
2023-07-22 11:03:33 +0000

    xhci: Treat not finding the event_seg on COMP_STOP the same as COMP_STOP_INVAL

--- word_embedding/data.py
+++ word_embedding/data.py
@@ -22,4 +22,77 @@
 
 __all__ = [
     'WikiDumpStream', 'preprocess_dataset', 'wiki', 'transform_data_fasttext',
-    'transform_data_word2vec
+    'transform_data_word2vec', 'skipgram_lookup', 'cbow_lookup',
+    'skipgram_fasttext_batch', 'cbow_fasttext_batch', 'skipgram_batch',
+    'cbow_batch']
+
+import functools
+import io
+import itertools
+import json
+import math
+import os
+import warnings
+
+import mxnet as mx
+import numpy as np
+
+import gluonnlp as nlp
+from gluonnlp import Vocab
+from gluonnlp.base import numba_njit
+from gluonnlp.data import CorpusDataset, SimpleDatasetStream
+from utils import print_time
+
+
+def preprocess_dataset(data, min_freq=5, max_vocab_size=None):
+    """Dataset preprocessing helper.
+
+    Parameters
+    ----------
+    data : mx.data.Dataset
+        Input Dataset. For example gluonnlp.data.Text8 or gluonnlp.data.Fil9
+    min_freq : int, default 5
+        Minimum token frequency for a token to be included in the vocabulary
+        and returned DataStream.
+    max_vocab_size : int, optional
+        Specifies a maximum size for the vocabulary.
+
+    Returns
+    -------
+    gluonnlp.data.DataStream
+        Each sample is a valid input to
+        gluonnlp.data.EmbeddingCenterContextBatchify.
+    gluonnlp.Vocab
+        Vocabulary of all tokens in Text8 that occur at least min_freq times of
+        maximum size max_vocab_size.
+    idx_to_counts : list of int
+        Mapping from token indices to their occurrence-counts in the Text8
+        dataset.
+
+    """
+    with print_time('count and construct vocabulary'):
+        counter = nlp.data.count_tokens(itertools.chain.from_iterable(data))
+        vocab = nlp.Vocab(counter, unknown_token=None, padding_token=None,
+                          bos_token=None, eos_token=None, min_freq=min_freq,
+                          max_size=max_vocab_size)
+        idx_to_counts = [counter[w] for w in vocab.idx_to_token]
+
+    def code(sentence):
+        return [vocab[token] for token in sentence if token in vocab]
+
+    with print_time('code data'):
+        data = data.transform(code, lazy=False)
+    data = nlp.data.SimpleDataStream([data])
+    return data, vocab, idx_to_counts
+
+
+def wiki(wiki_root, wiki_date, wiki_language, max_vocab_size=None):
+    """Wikipedia dump helper.
+
+    Parameters
+    ----------
+    wiki_root : str
+        Parameter for WikiDumpStream
+    wiki_date : str
+        Parameter for WikiDumpStream
+    wiki_langua
