656754409f95a2de572c509d0eb953d494fdecd9
liminat <180078053+liminat@users.noreply.github.com>
2023-08-05 16:59:20 +0000

    asserts

--- word_embedding/data.py
+++ word_embedding/data.py
@@ -493,3 +493,71 @@ def cbow_lookup(context_row, context_col, subwordidxs, subwordidxsptr,
             data.append(1 / (1 + end - start))
 
     # Normalize by number of words
+    for i, row_ in enumerate(row):
+        assert 0 <= row_ <= num_rows
+        data[i] /= row_to_numwords[row_]
+
+    return (np.array(data, dtype=np.float32), np.array(row, dtype=np.int64),
+            np.array(col, dtype=np.int64))
+
+
+class WikiDumpStream(SimpleDatasetStream):
+    """Stream for preprocessed Wikipedia Dumps.
+
+    Expects data in format
+    - root/date/wiki.language/*.txt
+    - root/date/wiki.language/vocab.json
+    - root/date/wiki.language/counts.json
+
+    Parameters
+    ----------
+    path : str
+        Path to a folder storing the dataset and preprocessed vocabulary.
+    skip_empty : bool, default True
+        Whether to skip the empty samples produced from sample_splitters. If
+        False, `bos` and `eos` will be added in empty samples.
+    bos : str or None, default None
+        The token to add at the beginning of each sentence. If None, nothing is
+        added.
+    eos : str or None, default None
+        The token to add at the end of each sentence. If None, nothing is
+        added.
+
+    Attributes
+    ----------
+    vocab : gluonnlp.Vocab
+        Vocabulary object constructed from vocab.json.
+    idx_to_counts : list[int]
+        Mapping from vocabulary word indices to word counts.
+
+    """
+
+    def __init__(self, root, language, date, skip_empty=True, bos=None,
+                 eos=None):
+        self._root = root
+        self._language = language
+        self._date = date
+        self._path = os.path.join(root, date, 'wiki.' + language)
+
+        if not os.path.isdir(self._path):
+            raise ValueError('{} is not valid. '
+                             'Please make sure that the path exists and '
+                             'contains the preprocessed files.'.format(
+                                 self._path))
+
+        self._file_pattern = os.path.join(self._path, '*.txt')
+        super(WikiDumpStream, self).__init__(
+            dataset=CorpusDataset, file_pattern=self._file_pattern,
+            skip_empty=skip_empty, bos=bos, eos=eos)
+
+    @property
+    def vocab(self):
+        path = os.path.join(self._path, 'vocab.json')
+        with io.open(path, 'r', encoding='utf-8') as in_file:
+            return Vocab.from_json(in_file.read())
+
+    @property
+    def idx_to_counts(self):
+        path = os.path.join(self._path, 'counts.json')
+        with io.open(path, 'r', encoding='utf-8') as in_file:
+            return
